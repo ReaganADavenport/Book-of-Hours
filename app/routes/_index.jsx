666d370a4faa27848349e59fe91ad792e2e9010c
Reagan Davenport <reagan.a.davenport@gmail.com>
2023-04-26 15:12:23 -0400

    trying and failing to abstract the styles out of index.jsx

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,11 +2,12 @@ export const meta = () => {
   return [{ title: "Book of Hours" }];
 };
 
+import styles from '../styles/main.css'
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "Canterbury", lineHeight: "1.4" }}>
-      <h1>Welcome to Book of Hours</h1>
+    <div style={{ fontFamily: "Canterbury", lineHeight: "1.8" }}>
+      <h1 style={{ fontSize:"40px"}}>Welcome to Book of Hours</h1>
       <p>I am the home page</p>
     </div>
   );
