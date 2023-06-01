05184db39e0b10c9a2635806620be015a979a143
Reagan Davenport <reagan.a.davenport@gmail.com>
2023-06-01 16:55:42 -0400

    more messing with styles before giving up, added links to other pages on homepage

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,7 +1,7 @@
 export const meta = () => {
   return [{ title: "Book of Hours" }];
 };
-
+import { Link } from '@remix-run/react'
 import styles from '../styles/main.css'
 
 export default function Index() {
@@ -9,6 +9,11 @@ export default function Index() {
     <div style={{ fontFamily: "Canterbury", lineHeight: "1.8" }}>
       <h1 style={{ fontSize:"40px"}}>Welcome to Book of Hours</h1>
       <p>I am the home page</p>
+      <Link to='/social'>Go to Social</Link>
+      <blockquote></blockquote>
+      <Link to='/personal'>Go to Personal</Link>
+      <blockquote></blockquote>
+      <Link to='/work'>Go to Work</Link>
     </div>
   );
 }
