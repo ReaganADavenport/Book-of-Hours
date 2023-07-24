796e1183b56933be888024a3986a93a5b8b230fc
Reagan Davenport <reagan.a.davenport@gmail.com>
2023-07-24 09:35:33 -0400

    created a components folder, created new files NewEntry.jsx and NewEntry.css, added basic form to NewEntry.jsx, (tried adding it to the css, but we don't talk about that)

---
+++ app/components/NewEntry.jsx
@@ -0,0 +1,20 @@
+function NewEntry() {
+    return(
+        <form method="post" className="entry-form">
+            <p>
+                <label htmlFor="title">Title</label>
+                <input type="text" className="title" name="title" required />
+            </p>
+            <p>
+                <label htmlFor="content">Content</label>
+                <textarea className="content" name="content" rows={5} required/>
+            </p>
+            <div className="form-actions">
+                <button>Add Entry</button>
+            </div>
+
+        </form>
+    );
+}
+
+export default NewEntry
